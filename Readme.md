1d3a834c4511b5e7528e324451123e953e281b7e
Kristian Mandrup <krma@netcompany.com>
2023-09-30 10:12:47 +0200

    Update

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # Sublime snippets to VS Code CLI
 
-Easily convert sublime snippet files to VS Code snippets.
+Easily convert sublime `xml` snippet files to VS Code `json` snippets.
 
 # Usage
 
@@ -8,7 +8,56 @@ Follow instructions from help page
 
 `subsnip2vsc -h`
 
-## Examples
+## Conversion example
+
+This tool can convert Sublime `xml` based snippet files to a VS Code `snippets.json` file. The tool aims to do 90% of the job with fine-tuning left to you.
+
+`dollarsign.sublime-snippet`
+
+```xml
+<snippet>
+	<content>
+		<![CDATA[\${$0}]]>
+	</content>
+	<tabTrigger>g</tabTrigger>
+	<scope>text.html.gsp</scope>
+	<description>${}</description>
+</snippet>
+```
+
+`gactionsubmit.sublime-snippet`
+
+```json
+<snippet>
+	<content><![CDATA[
+<g:actionSubmit value="${1:text}" action="${2:action}" ${3:onclick="${4:jsCode}"} />]]></content>
+	<tabTrigger>gactionsubmit</tabTrigger>
+	<scope>text.html.gsp</scope>
+	<description>Generates a submit button that maps to a specific action</description>
+</snippet>
+
+```
+
+Converted to a single VS Code `snippets.json` file
+
+```json
+{
+  "dollarsign": {
+    "body": "\\${$0}",
+    "description": "${}",
+    "prefix": "g"
+  },
+  "gactionsubmit": {
+    "body": "\r\n<g:actionSubmit value=\"${1:text}\" action=\"${2:action}\" ${3:onclick=\"${4:jsCode}\"} >",
+    "description": "Generates a submit button that maps to a specific action",
+    "prefix": "gactionsubmit"
+  }
+}
+```
+
+From there here you can fine tune the snippet file to suit your preferences.
+
+## Usage examples
 
 Convert all `*.tmSnippet` files in `snippets` folder and print output to screen
 
--- convert.js
+++ convert.js
@@ -1,4 +1,3 @@
-const JSON5 = require("json5");
 const fs = require("fs");
 const path = require("path");
 const convert = require("xml-js");
@@ -7,9 +6,8 @@ const convert = require("xml-js");
 const fg = require("fast-glob");
 
 const parse = (input) => {
-  const json = JSON5.parse(input);
+  const json = JSON.parse(input);
   const { name, body, description, prefix } = json;
-  console.log("PARSE", { bl: body?.length, pl: prefix?.length });
   return name && body?.length && prefix?.length
     ? {
         [name]: {
@@ -21,6 +19,48 @@ const parse = (input) => {
     : undefined;
 };
 
+export const convertEntry = (entry, options) => {
+  const filePath = path.join(options.cwd, entry);
+  console.log(`filePath: ${filePath}`);
+  let ext = path.extname(filePath);
+  let name = path.basename(entry, ext);
+  let data;
+  try {
+    data = fs.readFileSync(filePath, "utf8");
+  } catch (e) {
+    console.error(`Error reading file at ${filePath} : ${e.message}`);
+    return;
+  }
+
+  const decodeCDataOf = (elem) => elem.replace(/\//g, "");
+
+  const textOf = (elem) => {
+    return elem._text || decodeCDataOf(elem._cdata);
+  };
+
+  try {
+    data = convert.xml2json(data, { compact: true, spaces: 4 });
+    const json = JSON.parse(data);
+    const jsonObj = {
+      name
+    };
+    const snippet = json.snippet;
+    const content = textOf(snippet.content);
+    const description = textOf(snippet.description);
+    const trigger = textOf(snippet.tabTrigger);
+    // jsonObj.body = JSON.stringify(content.replace("\\\\", "\\"));
+    jsonObj.body = content.replace("\\\\", "\\");
+    jsonObj.description = description;
+    jsonObj.prefix = trigger;
+
+    // todo: convert to format expected by atomizr
+    data = JSON.stringify(jsonObj);
+  } catch (e) {
+    console.log(`Invalid XML: ${e.message}`);
+  }
+  return data;
+};
+
 module.exports = (pattern, options) => {
   const { output, print, dir, indent } = options;
   if (dir) {
@@ -28,53 +68,14 @@ module.exports = (pattern, options) => {
   }
   const patterns = Array.isArray(pattern) ? pattern : [pattern];
   const entries = fg.globSync(patterns, options);
-  console.log({ entries });
-  console.log({ patterns, pattern, options });
-
   let snippetsObj = {};
   try {
     entries.forEach((entry) => {
-      console.log(`converting: ${entry}`);
-      const filePath = path.join(options.cwd, entry);
-      console.log(`filePath: ${filePath}`);
-      let ext = path.extname(filePath);
-      let name = path.basename(entry, ext);
-      let data = fs.readFileSync(filePath, "utf8");
-      console.log(data);
-
-      const decodeCDataOf = (elem) => elem.replace(/\//g, "");
-
-      const textOf = (elem) => {
-        return elem._text || decodeCDataOf(elem._cdata);
-      };
-
-      try {
-        // data = convert.xml2json(data, { compact: false, spaces: 4 });
-        data = convert.xml2json(data, { compact: true, spaces: 4 });
-        console.log({ jsonData: data });
-        const json = JSON.parse(data);
-        console.log({ json });
-        const jsonObj = {
-          name
-        };
-        const snippet = json.snippet;
-        const content = textOf(snippet.content);
-        const description = textOf(snippet.description);
-        const trigger = textOf(snippet.tabTrigger);
-        // jsonObj.body = JSON.stringify(content.replace("\\\\", "\\"));
-        jsonObj.body = content.replace("\\\\", "\\");
-        jsonObj.description = description;
-        jsonObj.prefix = trigger;
-
-        console.log({ jsonObj });
-        // todo: convert to format expected by atomizr
-        data = JSON.stringify(jsonObj);
-      } catch (e) {
-        console.log("Skipping XML conversion");
+      const data = convertEntry(entry, options);
+      if (!data?.length) {
+        return;
       }
-      console.log("JSON:", data);
       const snippetJson = parse(data);
-      console.log({ snippetJson });
       if (print === true) {
         console.log("================================================");
         console.log(snippetJson);
@@ -86,6 +87,8 @@ module.exports = (pattern, options) => {
             ...snippetsObj,
             ...snippetJson
           };
+        } else {
+          console.log(`invalid snippet file skipped: ${entry}`);
         }
       }
     });
@@ -94,15 +97,14 @@ module.exports = (pattern, options) => {
     process.exit(1);
   }
   if (print === true) {
-    console.log("SKIP WRITING TO FILE");
     process.exit(0);
   }
   try {
-    console.log("WRITING TO FILE", output);
+    console.log("WRITING TO", output);
     const json = JSON.stringify(snippetsObj, null, indent);
-    console.log("FINAL", { json });
     fs.writeFileSync(output, json);
   } catch (error) {
     console.error(error);
+    process.exit(1);
   }
 };
